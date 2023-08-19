7058e8c125f11e4b2b6ee9e6b42ff660e46c0626
caglaaksoy <caglaaksoy2001@gmail.com>
2023-08-19 16:58:07 +0300

    BlogList start

---
+++ Frontend/PetsProject.WebUI/Controllers/BlogListController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using PetsProject.WebUI.Dtos.BlogDto;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+namespace PetsProject.WebUI.Controllers
+{
+    public class BlogListController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private string target;
+
+        public BlogListController(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        {
+            _httpClientFactory = httpClientFactory;
+            target = configuration["BackendTarget"];
+            if (string.IsNullOrEmpty(target))
+            {
+                throw new Exception("lütfen target değerini girin.");
+            }
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"{target}/api/BlogList");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBlogDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+       
+    }
+}
---
+++ Frontend/PetsProject.WebUI/ViewComponents/BlogList/_BlogListScriptsPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetsProject.WebUI.ViewComponents.BlogList
+{
+    public class _BlogListScriptsPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
