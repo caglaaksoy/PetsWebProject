f32a92ca225fda6a4da1e5d8f6142e91a1e23c91
caglaaksoy <caglaaksoy2001@gmail.com>
2023-08-23 18:25:54 +0300

    Member Layout start

---
+++ Frontend/PetsProject.WebUI/Controllers/MemberHomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetsProject.WebUI.Controllers
+{
+    public class MemberHomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontend/PetsProject.WebUI/Controllers/MemberLayoutController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetsProject.WebUI.Controllers
+{
+    public class MemberLayoutController : Controller
+    {
+        public IActionResult _MemberLayout()
+        {
+            return View();
+        }
+
+        public PartialViewResult HeadMemberPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult HeaderMemberPartial()
+        {
+            return PartialView();
+        }
+        //public PartialViewResult SidebarMemberPartial()
+        //{
+        //    return PartialView();
+        //}
+
+        //public PartialViewResult FooterMemberPartial()  //FOREACH HATASI İÇİN SONRA BAK
+        //{
+        //    return PartialView();
+        //}
+
+        public PartialViewResult ScriptMemberPartial()
+        {
+            return PartialView();
+        }
+
+
+    }
+}
